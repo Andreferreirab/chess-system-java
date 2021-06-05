2aa17c918e8a520c2c19f3bfc412e16a9fa2755f
Andre Ferreira <andreferreirab@gmail.com>
2021-06-05 18:26:37 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,7 +2,7 @@ package boardgame;
 
 // Os atributos e métodos protected o são para que sejam visíveis somente pela camada boardgame, e não por outras camadas, no caso a camada de xadrez, e também pelas subclasses de peças.
 
-public class Piece {
+public abstract class Piece {
 	protected Position position;
 	private Board board;
 	
@@ -13,5 +13,25 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
-	}	
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove (Position position) {
+		//hook methods: método concerto fazendo uso de um método abastrato
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		
+		for (int i = 0; i < mat.length ; i++ ) {
+			for (int j = 0; j < mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
